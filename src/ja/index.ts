45c540513758fb972ff7be93f810782151e39158
kichion <kkikuchi1539@gmail.com>
2021-06-08 19:53:23 +0900

    :sparkles: Add "明日" date

--- src/ja/index.ts
+++ src/ja/index.ts
@@ -1,3 +1,16 @@
-export const whenToDate = (when: string): Date => {
-  return new Date();
+export type When = "今日" | "明日";
+
+export const whenToDate = (when: When): Date => {
+  return getAddedDate(whenMapping[when]);
+};
+
+const whenMapping: { [key in When]: number } = {
+  ["今日"]: 0,
+  ["明日"]: 1,
+};
+
+const getAddedDate = (add: number): Date => {
+  const targetDate = new Date();
+  targetDate.setDate(targetDate.getDate() + add);
+  return targetDate;
 };
--- tests/ja/index.test.ts
+++ tests/ja/index.test.ts
@@ -1,21 +1,20 @@
+import { advanceBy, advanceTo, clear } from "jest-date-mock";
 import { whenToDate } from "#/ja";
 
 describe("test", () => {
-  let now: Date;
-  let spiedDate: jest.SpyInstance<string, []>;
   beforeAll(() => {
-    const OriginalDate = Date; // 退避
-    now = new OriginalDate("2019/8/1 12:00:00");
-    // Date.now() と new Date() のみmocking
-    Date.now = jest.fn().mockReturnValue(now.valueOf());
-    spiedDate = jest
-      .spyOn(global, "Date")
-      .mockImplementation(() => now as unknown as string);
+    advanceTo(new Date("2019/8/1 12:00:00"));
   });
   afterAll(() => {
-    spiedDate.mockRestore();
+    clear();
   });
-  test("today", () => {
-    expect(whenToDate("今日")).toBe(new Date());
+  test("今日は本日を返す", () => {
+    expect(whenToDate("今日")).toEqual(new Date());
+  });
+
+  test("明日は翌日を返す", () => {
+    const targetDate = new Date();
+    targetDate.setDate(targetDate.getDate() + 1);
+    expect(whenToDate("明日")).toEqual(targetDate);
   });
 });

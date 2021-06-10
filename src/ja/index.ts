a29fa5d4e6e645390e0453deeb962d4324bb9d6f
kichion <kkikuchi1539@gmail.com>
2021-06-10 18:34:25 +0900

    :sparkles: Add "昨日" / "一昨日" / "明後日" date

--- src/ja/index.ts
+++ src/ja/index.ts
@@ -1,12 +1,15 @@
-export type When = "今日" | "明日";
+export type When = "一昨日" | "昨日" | "今日" | "明日" | "明後日";
 
 export const whenToDate = (when: When): Date => {
   return getAddedDate(whenMapping[when]);
 };
 
 const whenMapping: { [key in When]: number } = {
-  ["今日"]: 0,
+  ["明後日"]: 2,
   ["明日"]: 1,
+  ["今日"]: 0,
+  ["昨日"]: -1,
+  ["一昨日"]: -2,
 };
 
 const getAddedDate = (add: number): Date => {
--- tests/ja/index.test.ts
+++ tests/ja/index.test.ts
@@ -17,4 +17,22 @@ describe("test", () => {
     targetDate.setDate(targetDate.getDate() + 1);
     expect(whenToDate("明日")).toEqual(targetDate);
   });
+
+  test("昨日は前日を返す", () => {
+    const targetDate = new Date();
+    targetDate.setDate(targetDate.getDate() - 1);
+    expect(whenToDate("昨日")).toEqual(targetDate);
+  });
+
+  test("明後日は2日後を返す", () => {
+    const targetDate = new Date();
+    targetDate.setDate(targetDate.getDate() + 2);
+    expect(whenToDate("明後日")).toEqual(targetDate);
+  });
+
+  test("一昨日は2日前を返す", () => {
+    const targetDate = new Date();
+    targetDate.setDate(targetDate.getDate() - 2);
+    expect(whenToDate("一昨日")).toEqual(targetDate);
+  });
 });
